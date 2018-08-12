a89d359ce4418933ad02e869b886515184057f8b
Italo Felipe <felipe.italo@hotmail.com>
2018-08-12 18:22:15 -0300

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => { // Esse método recebe 2 argumentos: O primeiro é uma string, 
+// a URL de onde a base de dados está (pode ser local ou remota). O segundo argumento é uma função callback
+// o callback será disparado se a conxão tiver sido feita ou nao.
+   if (err) {
+      console.log('Unable to connect to Mongo DB server');
+   }
+   console.log('Connected to MongoDB Server');
+   
+   db.collection('To-Dos').insertOne({
+      text: 'Something to do',
+      completed: false
+   }, (err, result) => {
+      if (err) {
+         return console.log('Unable to insert to-do', err);
+      }
+      console.log(JSON.stringify(result.ops));
+      
+   });
+   
+
+   db.collection('Users').insertOne({
+      name: 'Italo',
+      age: 20,
+      location: 'Riacho Fundo'
+   }, (err, result) => {
+      if (err) {
+         return console.log('Unable to insert to-do', err);
+      }
+      console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+   })
+   db.close();
+}); 
---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+   if (err) {
+      console.log('Unable to connect to Mongo DB server');
+   }
+   console.log('Connected to MongoDB Server');
+   
+   db.collection('Users').find({
+      name: 'Italo'
+   }).toArray().then((docs) => {
+      console.log('To-dos ');
+      console.log(JSON.stringify(docs, undefined, 2))
+      
+   }, (err) => {
+      console.log('Unable to fetch to-dos', err)
+   })
+
+
+   db.collection('To-Dos').find().count().then((count) => {
+      // console.log(`To-dos count: ${count}`);
+      
+   }, (err) => {
+      console.log('Unable to fetch to-dos', err)
+   })
+
+   // db.close();
+});
+
